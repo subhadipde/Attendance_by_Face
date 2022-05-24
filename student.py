9448bdfcf42b5e4b11cf04abd975638f83a54e4d
Subhadip De <88263132+subhadipde@users.noreply.github.com>
2022-05-24 22:07:07 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,11 +1,9 @@
-from cmath import e
-import collections
-from textwrap import fill
 from tkinter import*
 from tkinter import ttk
 from tkinter import messagebox
 import customtkinter as ctk
 from PIL import Image, ImageTk
+import mysql.connector
 
 # custom tkinter setting
 ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
@@ -37,14 +35,14 @@ class Student:
         # Variables -------------------------------------------------
 
         self.dep = StringVar()
-        self.year = StringVar()
+        self.batch = StringVar()
         self.sem = StringVar()
         self.id = StringVar()
         self.name = StringVar()
-        self.roll = StringVar()
         self.gender = StringVar()
-        self.dob = StringVar()
         self.phone = StringVar()
+        self.dob = StringVar()
+        self.add = StringVar()
         self.search = StringVar()
         self.search_entry = StringVar()
 
@@ -58,7 +56,7 @@ class Student:
         bg_img=Label(self.root, image=self.photoimg3)
         bg_img.place(width=Width,height=Height)
 
-        title_lbl=ctk.CTkLabel(root, text="STUDET MANAGEMENT SYSTEM", bg_color="black")
+        title_lbl=ctk.CTkLabel(root, text="STUDENT MANAGEMENT SYSTEM")
         title_lbl.configure(font=("Lato",25))
         title_lbl.place(relx=0.5, rely=0.05, anchor=CENTER)
 
@@ -90,7 +88,7 @@ class Student:
         current_course_frame.place(relx=0.5, rely=0, anchor=N)
 
         # Department
-        dep_label=ctk.CTkLabel(current_course_frame, text="Department")
+        dep_label=ctk.CTkLabel(current_course_frame, text="Department*")
         dep_label.configure(font=("Lato",15))
         dep_label.place(relx=0.2, rely=0.2, anchor=NE)
 
@@ -100,17 +98,17 @@ class Student:
         dep_combo.place(relx=0.4, rely=0.2, anchor=N)
 
         # # Year
-        year_label=ctk.CTkLabel(current_course_frame, text="Year")
+        year_label=ctk.CTkLabel(current_course_frame, text="Batch*")
         year_label.configure(font=("Lato",15))
         year_label.place(relx=0.7, rely=0.7, anchor=E)
 
-        year_combo=ttk.Combobox(current_course_frame, textvariable=self.year, font=("times new roman",12,"bold"), state="readonly")
-        year_combo["values"]=("Select Year", "2019-20","2020-21", "2021-22","2022-23")
+        year_combo=ttk.Combobox(current_course_frame, textvariable=self.batch, font=("times new roman",12,"bold"), state="readonly")
+        year_combo["values"]=("Select Batch", "2018","2019", "2020","2021")
         year_combo.current(0)
         year_combo.place(relx=0.8, rely=0.6, anchor=N)
 
         # #Semester
-        semester_label=ctk.CTkLabel(current_course_frame, text="Semester")
+        semester_label=ctk.CTkLabel(current_course_frame, text="Semester*")
         semester_label.configure(font=("Lato",15))
         semester_label.place(relx=0.2, rely=0.7, anchor=E)
 
@@ -128,7 +126,7 @@ class Student:
         class_Student_frame.place(relx=0.5, rely=1, anchor=S)
 
         # # student id
-        studentId_label=ctk.CTkLabel(class_Student_frame, text="StudentID: ")
+        studentId_label=ctk.CTkLabel(class_Student_frame, text="StudentID*: ")
         studentId_label.configure(font=("Lato",14))
         studentId_label.place(relx=0.2, rely=0.2, anchor=E)
 
@@ -142,7 +140,7 @@ class Student:
         studentID_entry.place(relx=0.4, rely=0.2, anchor=CENTER)
 
         # # student name
-        studenName_label=ctk.CTkLabel(class_Student_frame, text="Student Name: ")
+        studenName_label=ctk.CTkLabel(class_Student_frame, text="Student Name*: ")
         studenName_label.configure(font=("Lato",14))
         studenName_label.place(relx=0.25, rely=0.35, anchor=E)
 
@@ -155,19 +153,19 @@ class Student:
                                corner_radius=10)
         studentName_entry.place(relx=0.4, rely=0.35, anchor=CENTER)
 
-        # # Roll No
-        roll_no_label=ctk.CTkLabel(class_Student_frame, text="Roll No: ")
-        roll_no_label.configure(font=("Lato",14))
-        roll_no_label.place(relx=0.20, rely=0.5, anchor=E)
+        # # Address
+        add_label=ctk.CTkLabel(class_Student_frame, text="Address*: ")
+        add_label.configure(font=("Lato",14))
+        add_label.place(relx=0.20, rely=0.5, anchor=E)
 
-        roll_no_entry = ctk.CTkEntry(class_Student_frame,
-                               textvariable=self.roll,
-                               placeholder_text="Enter Roll here",
-                               width=220,
+        add_entry = ctk.CTkEntry(class_Student_frame,
+                               textvariable=self.add,
+                               placeholder_text="Enter Address here",
+                               width=450,
                                height=40,
                                border_width=1,
                                corner_radius=10)
-        roll_no_entry.place(relx=0.4, rely=0.5, anchor=CENTER)
+        add_entry.place(relx=0.5, rely=0.5, anchor=CENTER)
 
         # # Gender
         gender_label=ctk.CTkLabel(class_Student_frame, text="Gender: ")
@@ -209,24 +207,24 @@ class Student:
         phone_entry.place(relx=0.8, rely=0.65, anchor=CENTER)
 
         # #buttons
-
+        
         save_btn=ctk.CTkButton(class_Student_frame,command=self.add_data, text="Save", cursor="hand2")
-        save_btn.place(relx=0.20, rely=0.85,width=160,height=60,  anchor=S)
+        save_btn.place(relx=0.15, rely=0.85,width=160,height=60,  anchor=S)
 
         update_btn=ctk.CTkButton(class_Student_frame, text="Update", cursor="hand2")
-        update_btn.place(relx=0.40, rely=0.85,width=160,height=60,  anchor=S)
+        update_btn.place(relx=0.35, rely=0.85,width=160,height=60,  anchor=S)
 
-        delete_btn=ctk.CTkButton(class_Student_frame, text="Delete", cursor="hand2", fg_color="red")
-        delete_btn.place(relx=0.60, rely=0.85,width=160,height=60,  anchor=S)
+        delete_btn=ctk.CTkButton(class_Student_frame, text="Delete", cursor="hand2", fg_color="red", text_font=("bold"))
+        delete_btn.place(relx=0.65, rely=0.85,width=160,height=60,  anchor=S)
 
         reset_btn=ctk.CTkButton(class_Student_frame, command=self.reset_data, text="Reset", cursor="hand2")
-        reset_btn.place(relx=0.80, rely=0.85,width=160,height=60,  anchor=S)
+        reset_btn.place(relx=0.85, rely=0.85,width=160,height=60,  anchor=S)
 
         take_photo_btn=ctk.CTkButton(class_Student_frame, text="Take Photo", cursor="hand2")
-        take_photo_btn.place(relx=0.3, rely=0.95,width=400,height=60,  anchor=S)
+        take_photo_btn.place(relx=0.3, rely=0.98,width=300,height=60,  anchor=S)
 
         update_photo_btn=ctk.CTkButton(class_Student_frame, text="Update Photo", cursor="hand2")
-        update_photo_btn.place(relx=0.7, rely=0.95,width=400,height=60,  anchor=S)
+        update_photo_btn.place(relx=0.7, rely=0.98,width=300,height=60,  anchor=S)
 
         # # Right label frame
         Right_frame = ctk.CTkFrame(main_frame,
@@ -260,13 +258,13 @@ class Student:
 
         # # # ===========================Table Frame=======================================================
         table_frame=Frame(Right_frame, bd=2, bg="white", relief=RIDGE)
-        table_frame.place(relx=0.5, rely=0.9,width=wt/2+100,height=ht-100,  anchor=S)
+        table_frame.place(relx=0.5, rely=0.9,width=wt/2,height=ht-100,  anchor=S)
         
 
         scroll_x=ttk.Scrollbar(table_frame, orient=HORIZONTAL)
         scroll_y=ttk.Scrollbar(table_frame, orient=VERTICAL)
 
-        self.student_table=ttk.Treeview(table_frame, columns=("dep","year","sem","id","name","roll","gender","dob","phone","photo"), xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
+        self.student_table=ttk.Treeview(table_frame, columns=("dep","batch","sem","id","name","gender","phone","dob","add"), xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
 
         scroll_x.pack(side=BOTTOM, fill=X)
         scroll_y.pack(side=RIGHT, fill=Y)
@@ -274,52 +272,96 @@ class Student:
         scroll_y.config(command=self.student_table.yview)
 
         self.student_table.heading("dep", text="Department")
-        self.student_table.heading("year", text="Year")
+        self.student_table.heading("batch", text="Batch")
         self.student_table.heading("sem", text="Semester")
-        self.student_table.heading("id", text="StudentId")
+        self.student_table.heading("id", text="Id")
         self.student_table.heading ("name", text="Name")
-        self.student_table.heading ("roll", text="Roll")
         self.student_table.heading ("gender", text="Gender")
-        self.student_table.heading("dob", text="DOB")
         self.student_table.heading("phone", text="Phone")
-        self.student_table.heading("photo", text="Photo")
+        self.student_table.heading("dob", text="DOB")
+        self.student_table.heading ("add", text="Address")
+        # self.student_table.heading("photo", text="Photo")
         self.student_table["show"]="headings"
 
-        self.student_table.column("dep", width=120)
-        self.student_table.column("year", width=100)
-        self.student_table.column("sem", width=90)
-        self.student_table.column("id", width=100)
-        self.student_table.column("name", width=100)
-        self.student_table.column("roll", width=70)
-        self.student_table.column("gender", width=70)
-        self.student_table.column("dob", width=80)
-        self.student_table.column("phone", width=80)
-        self.student_table.column("photo", width=60)
+        self.student_table.column("dep", width=95)
+        self.student_table.column("batch", width=90)
+        self.student_table.column("sem", width=80)
+        self.student_table.column("id", width=35)
+        self.student_table.column("name", width=150)
+        self.student_table.column("gender", width=65)
+        self.student_table.column("phone", width=90)
+        self.student_table.column("dob", width=90)
+        self.student_table.column("add", width=150)
+        # self.student_table.column("photo", width=60)
 
         self.student_table.pack(fill=BOTH,expand=1)
+        self.student_table.bind("<ButtonRelease>",self.get_cursor)
+        self.fetch_data()
 
 
-         # Exit button        
-
-        b6_1=ctk.CTkButton(Right_frame,text="Exit", cursor="hand2", command = root.destroy, fg_color="red", text_font=("Verdana", 10))
+        # # Exit button        
+        b6_1=ctk.CTkButton(Right_frame,text="Exit", cursor="hand2", command = root.destroy, fg_color="red", text_font=("Verdana",13,"bold"))
         b6_1.place(relx=0.8, rely=0.95,width=200,height=60,  anchor=CENTER)
 
 
     # -----------------function works-----------------------------------------------------------------------
     def add_data(self):
-        if self.dep.get()=="Select Department" or self.year.get()=="Select Year" or self.sem.get()=="Select Semester" or self.id.get()=="" or self.name.get()=="" or self.roll.get()=="" or self.dob.get()=="" or self.gender.get()=="Select" or self.phone.get()=="":
-            messagebox.showerror("Error", "All fields are required!", parent=self.root)
+        if self.dep.get()=="Select Department" or self.batch.get()=="Select Batch" or self.sem.get()=="Select Semester" or self.id.get()=="" or self.add.get()=="":
+            messagebox.showerror("Error", "All (*) fields are required!", parent=self.root)
         else:
-            messagebox.showinfo("Success", "Record Saved successfully.")
-            self.dep.set("Select Department")
-            self.year.set("Select Year")
-            self.sem.set("Select Semester")
-            self.id.set("")
-            self.name.set("")
-            self.roll.set("")
-            self.dob.set("")
-            self.gender.set("Select")
-            self.phone.set("")
+            try:
+                conn=mysql.connector.connect(host="localhost", username="root", password="Subhadip@321#", database="face_recognition")
+                my_cursor=conn.cursor()
+                my_cursor.execute("insert into student values(%s,%s,%s,%s,%s,%s,%s,%s,%s)",(
+                                                                                            self.dep.get(),
+                                                                                            self.batch.get(),
+                                                                                            self.sem.get(),
+                                                                                            self.id.get(),
+                                                                                            self.name.get(),
+                                                                                            self.gender.get(),
+                                                                                            self.phone.get(),
+                                                                                            self.dob.get(),
+                                                                                            self.add.get()
+
+                                                                                        ))
+                conn.commit()
+                self.fetch_data()
+                conn.close()
+                messagebox.showinfo("Success","The Student Data has saved", parent=self.root)
+            except Exception as es:
+                messagebox.showerror("Error",f"Due To :{str(es)}", parent=self.root)
+
+    # ===============fetch Data====================
+    def fetch_data(self):
+        conn=mysql.connector.connect(host="localhost", username="root", password="Subhadip@321#", database="face_recognition")
+        my_cursor=conn.cursor()
+        my_cursor.execute("select * from student")
+        data=my_cursor.fetchall()
+
+        if len(data) != 0:
+            self.student_table.delete(*self.student_table.get_children())
+            for i in data:
+                self.student_table.insert("",END,values=i)
+            conn.commit()
+        conn.close()
+    
+
+    # ===============get cursor==================
+
+    def get_cursor(self, event=""):
+        cursor_focus=self.student_table.focus()
+        content=self.student_table.item(cursor_focus)
+        data=content["values"]
+
+        self.dep.set(data[0]),
+        self.batch.set(data[1]),
+        self.sem.set(data[2]),
+        self.id.set(data[3]),
+        self.name.set(data[4]),
+        self.gender.set(data[5]),
+        self.phone.set(data[6]),
+        self.dob.set(data[7]),
+        self.add.set (data[8])
 
     def reset_data(self):
         self.dep.set("Select Department")
